a4df798b5c9593e161800968f9cda384064d6e62
Sobia <sobiacodes@gmail.com>
2023-08-27 22:11:08 +0100

    minor error corrected

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Footer from './components/Footer';
 import GameContainer from './containers/GameContainer';
-import '/Users/sobiaahmad/codeclan_work/personal_projects/tic-tac-toe-game-updated/src/components/App.css';
-// import './App.css';
+import "./App.css";
+// import '/Users/sobiaahmad/codeclan_work/personal_projects/tic-tac-toe-game-updated/src/components/App.css';
 
 
 function App() {
